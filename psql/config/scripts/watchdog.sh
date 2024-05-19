85fc1fc82c132d8ac8d57bf15d5dd4cb8488220f
Jose <jose-pr@coqui.dev>
2024-05-18 22:49:32 -0400

    Update watchdog

--- psql/config/scripts/watchdog.sh
+++ psql/config/scripts/watchdog.sh
@@ -1,33 +1,37 @@
 source /etc/pve/psql/profile
 set -e
 
-if ! systemctl is-active postgresql > /dev/null;then
-    echo "PSQL disabled skipping checks"
+
+if ! is_quorate;then
+    echo "Dont run PSQL as we dont have quorum"
+    sudo systemctl stop postgresql repmgrd pgbouncer
+    exit 0
 fi
 
-IS_PVE_PRIMARY=$(jq ".master_node == \"`hostname`\"" /etc/pve/ha/manager_status)
-PVE_PRIMARY=$(jq ".master_node" /etc/pve/ha/manager_status)
-PSQL_PRIMARY_HOST=`get_psql_primary`
-IS_PSQL_PRIMARY=false
-for host in `hostname -i` `hostname` `hostname -f`;do
-    if [ "$PSQL_PRIMARY_HOST" = "$host" ]; then
-        IS_PSQL_PRIMARY=true
-        break
+for service in postgresql pgbouncer;do
+    if ! systemctl is-active "$service" > /dev/null;then
+        sudo systemctl start "$service"
     fi
 done
+
+IS_PVE_PRIMARY=`is_pve_primary && echo 'true' || echo 'false'`
+PVE_PRIMARY=`current_pve_primary`
+PSQL_PRIMARY_HOST=`current_psql_primary`
+IS_PSQL_PRIMARY=`is_psql_primary && echo 'true' || echo 'false'`
 
 parse_repmgr_csv 'REPMGR_' <<< "$(repmgr node status --csv 2> /dev/null)" || true  
 
 echo "Current PVE Primary is: $PVE_PRIMARY"
-echo "Current Primary Host is: $PSQL_PRIMARY_HOST"
+echo "Current PDQL Primary is: $PSQL_PRIMARY_HOST"
 echo "This node is primary for pve: $IS_PVE_PRIMARY psql: $IS_PSQL_PRIMARY"
 echo "Assigned PSQL Role: $REPMGR_ROLE"
 
 if  [ "$REPMGR_ROLE" = 'primary' ] && [ "$IS_PSQL_PRIMARY" = "false" ]; then
     echo "Failed Node, syncing to true primary"
-    sudo systemctl stop postgresql
+    sudo systemctl stop postgresql repmgrd
     rm -f "$PSQL_DATA/recovery.conf"
     repmgr node rejoin -d "host=$PSQL_PRIMARY_HOST dbname=repmgr user=repmgr" --force-rewind
+    sudo systemctl start postgresql repmgrd
 fi
 
 if  [ "$IS_PVE_PRIMARY" = 'true' ] && [ "$IS_PSQL_PRIMARY" = 'false' ]; then
@@ -38,8 +42,4 @@ fi
 if ! systemctl is-active repmgrd > /dev/null;then
     echo "Starting repmgrd"
     sudo systemctl start repmgrd
-fi
-if ! systemctl is-active pgbouncer > /dev/null;then
-    echo "Starting repmgrd"
-    sudo systemctl start pgbouncer
 fi
